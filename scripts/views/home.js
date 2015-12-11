ca01ce86a38d0fe3027817edcc8b6e3429f0797e
climbrick <climbrick@squarespace.com>
2015-12-11 16:30:19 -0500

    Ajax call to retrieve data and display it in the front end - Works great.

--- scripts/views/home.js
+++ scripts/views/home.js
@@ -1,59 +1,42 @@
 var Home = (function() {
-	var foo
-	var twitterData = [
-		{
-			user: {
-				profile_image_url : "assets/avatar.png",
-				name : "@Hodor"
-			},
-			text : "Hodor Hodor Hodor Hodor Hodor Hodor Hodor..... Hodor"
-		},
-		{
-			user: {
-				profile_image_url : "assets/avatar.png",
-				name : "@johnsnow"
-			},
-			text : "Someone once said that I know nothing..."
-		},
-		{
-			user: {
-				profile_image_url : "assets/avatar.png",
-				name : "@drwho"
-			},
-			text : "I love Squarespace Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sapien lacus, dignissim sagittis metus ac, imperdiet maximus"
-		}
-	]
-
 
 	// DOM Handlers
-	function startTimeout(i) {
-        foo = twitterData[i];
-	    setHTML();
+
+	// AJAX call to retrieve data from backend
+	function getData() {
+	    $.ajax({
+	        url: "/getTwitterInfo",
+	        success: function(data) {
+			  	console.log(data);
+			  	for (var i = 0; i < data.tweets.statuses.length; i++) {
+			  		var tweet = data.tweets.statuses[i];
+					setTimeout(startTimeout.bind(this,tweet.text, tweet.user.screen_name, tweet.user.profile_image_url), 3000 * i);
+			  	}
+			},
+	        dataType: "json"
+        });
+    }
+
+	// Starts the timeout function 
+	function startTimeout(text, screen_name, avatar ) {
+	    setHTML(text, screen_name, avatar);
 	    fadeInData();
       }
 
-	function setHTML() {
-		document.querySelector(".activeTweet__message").innerHTML = foo.text;
-		document.querySelector(".activeTweet__name").innerHTML = foo.user.name;
-		document.querySelector('.activeTweet__avatar img').src = foo.user.profile_image_url;
+    // Sets the tweet data to html elements
+	function setHTML(text, screen_name, avatar) {
+		document.querySelector(".activeTweet__message").innerHTML = text;
+		document.querySelector(".activeTweet__name").innerHTML = screen_name;
+		document.querySelector('.activeTweet__avatar img').src = avatar;
 	}
 
+	// Fades in Twitter data 
 	function fadeInData() {
 		$('.activeTweet').hide().fadeIn(900);
 	}
 
-	for (var i = 0; i < twitterData.length; i++) {
-		if (i === 0) {
-			foo = twitterData[i];
-			setHTML();
-			fadeInData();
-		}
-		else {
-	      // using setTimeout and incrementing the delay by count
-	      setTimeout(startTimeout.bind(this,i), 3000 * i);
-		}
-	}
-
+	// Starts getData() which retrieves and displays Twitter data 
+	getData();
 
 	// KICKSTART VIEW
 	function initHome() {
