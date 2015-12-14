6844761ccd6830d4fd49701a3c7c5ac4e89af080
climbrick <climbrick@squarespace.com>
2015-12-14 17:31:47 -0500

    adding gesture control to work as pagination for tweets

--- scripts/views/home.js
+++ scripts/views/home.js
@@ -15,7 +15,7 @@ var Home = (function() {
 
 				// stagger the timeouts
 			  	for (var i = 0; i < data.tweets.statuses.length; i++) {
-					setTimeout(startTimeout.bind(this, i, data.tweets.statuses.length), 5000 * i);
+					setTimeout(startTimeout.bind(this, i, data.tweets.statuses.length), 30000 * i);
 			  	}
 			},
 	        dataType: "json"
@@ -25,13 +25,13 @@ var Home = (function() {
 	// Starts the timeout function 
 	function startTimeout(i, totalTweets ) {
 		if ( i === totalTweets - 1 ) {
-			setTimeout(getData, 5000);
+			setTimeout(getData, 30000);
 		}
 		currentlyViewingIndex = i;
 
 		// Formatting Date
 		var tweetDate = moment(allTweets[currentlyViewingIndex].created_at).format( 'MMMM Do YYYY', 'en');
-		
+
 	    setHTML(allTweets[currentlyViewingIndex].text, allTweets[currentlyViewingIndex].user.screen_name, allTweets[currentlyViewingIndex].user.profile_image_url, tweetDate );
 	    fadeInData();
       }
@@ -50,13 +50,24 @@ var Home = (function() {
 	}
 
 	function motionDetection() {
-		currentlyViewingIndex = currentlyViewingIndex -1;
-		currentlyViewingIndex = currentlyViewingIndex +1;
-		startTimeout(currentlyViewingIndex, allTweets.length);
+		gest.start();
+		// Testing gesture control with gest.js
+		gest.options.subscribeWithCallback(function(gesture) {
+		    if (gesture.left) {
+				currentlyViewingIndex = currentlyViewingIndex -1;
+				startTimeout(currentlyViewingIndex, allTweets.length);
+		    }
+		    else if (gesture.right) {
+		    	currentlyViewingIndex = currentlyViewingIndex +1;
+		    	startTimeout(currentlyViewingIndex, allTweets.length);
+		    }
+		});
 	}
 
 	// Starts getData() which retrieves and displays Twitter data 
 	getData();
+	motionDetection();
+	// gest.options.debug(true);
 
 	// KICKSTART VIEW
 	function initHome() {
